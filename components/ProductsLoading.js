b4afe2dec5b1491fb7d0f3534b3f763b913ed31e
Dibitybopty <lee9t8@hotmail.com>
2020-12-08 15:04:57 +0000

    attempt overflow fix

--- components/ProductsLoading.js
+++ components/ProductsLoading.js
@@ -1,86 +1,93 @@
 import { motion } from "framer-motion";
 import utilStyles from '../styles/utils.module.css'
 
-export default function ProductsLoading(){
+export default function ProductsLoading() {
 
-    function moveCard(e) {
-        const card = e.currentTarget;
-        if (card.attributes.type !== undefined) {
-          if (card.attributes.type.value === 'cardContainer') {
-            //minus the scroll position of the screen so it's always the same value no matter how far scrolled
-            let scrollPositionProduct = card.parentElement.parentElement.scrollTop;
-    
-            let xAxisProduct = ((card.offsetLeft + (card.offsetWidth / 2)) - e.clientX) / 50;
-            let yAxisProduct = ((card.offsetTop + (card.offsetHeight / 2) - scrollPositionProduct) - e.clientY) / 50;
-            //use currentTarget offsetLeft somehow to fix jaggy? | fixed when paired with scroll position above! :D
-    
-            card.firstChild.style.transform = `rotateY(${xAxisProduct}deg) rotateX(${-yAxisProduct}deg)`
-            card.firstChild.style.transition = ''
-    
-            //console.log(card.parentElement.parentElement.scrollTop);
-    
-            //console.log(e.pageX)
-    
-          }
-        }
+  function moveCard(e) {
+    const card = e.currentTarget;
+    if (card.attributes.type !== undefined) {
+      if (card.attributes.type.value === 'cardContainer') {
+        //minus the scroll position of the screen so it's always the same value no matter how far scrolled
+        let scrollPositionProduct = card.parentElement.parentElement.scrollTop;
+
+        let xAxisProduct = ((card.offsetLeft + (card.offsetWidth / 2)) - e.clientX) / 50;
+        let yAxisProduct = ((card.offsetTop + (card.offsetHeight / 2) - scrollPositionProduct) - e.clientY) / 50;
+        //use currentTarget offsetLeft somehow to fix jaggy? | fixed when paired with scroll position above! :D
+
+        card.firstChild.style.transform = `rotateY(${xAxisProduct}deg) rotateX(${-yAxisProduct}deg)`
+        card.firstChild.style.transition = ''
+
+        //console.log(card.parentElement.parentElement.scrollTop);
+
+        //console.log(e.pageX)
+
       }
-    
-    
-      function leaveCard(e) {
-        const card = e.currentTarget;
-        card.firstChild.style.transition = 'ease 0.5s'
-        card.firstChild.style.transform = 'rotateY(0deg) rotateX(0deg)'
+    }
+  }
+
+
+  function leaveCard(e) {
+    const card = e.currentTarget;
+    card.firstChild.style.transition = 'ease 0.5s'
+    card.firstChild.style.transform = 'rotateY(0deg) rotateX(0deg)'
+  }
+
+  const cardAnimations = {
+    animate: {
+      transition: {
+        staggerChildren: 0.15,
+        delayChildren: 0.35,
+
       }
-    
-      const cardAnimations = {
-        animate: {
-          transition: {
-            staggerChildren: 0.15,
-            delayChildren: 0.35,
-    
-          }
-        },
-        exit: {
-          transition: {
-            staggerChildren: 0.15,
-            delayChildren: 0,
-    
-          }
-        },
-        initial: {
-          transition: {
-            staggerChildren: 0.15,
-            delayChildren: 0.7,
-    
-          }
-        }
+    },
+    exit: {
+      transition: {
+        staggerChildren: 0.15,
+        delayChildren: 0,
+
       }
-    
-      const cards = {
-        animate: {
-          y: 0,
-          transition: {
-            duration: 1
-          }
-    
-        },
-        initial: {
-          y: 1000,
-    
-        },
-        exit: {
-          y: 1000,
-          transition: {
-            duration: 1.7,
-            delayChildren: 0
-          }
-    
-        }
+    },
+    initial: {
+      transition: {
+        staggerChildren: 0.15,
+        delayChildren: 0.7,
+
       }
-      
-    return (
-        <>
-        <motion.div variants={cards} onPointerMove={(e) => moveCard(e)} onPointerLeave={(e) => leaveCard(e)} type='cardContainer' className={utilStyles.productCardContainer}>
+    }
+  }
+
+  const cards = {
+    animate: {
+      y: 0,
+      transition: {
+        duration: 1
+      }
+
+    },
+    initial: {
+      y: 1000,
+
+    },
+    exit: {
+      y: 1000,
+      transition: {
+        duration: 1.7,
+        delayChildren: 0
+      }
+
+    }
+  }
+
+  return (
+    <>
+
+      {/* <div className={utilStyles.loadingProducts}>
+        <img src='/images/products/loading-products.svg'></img>
+        <div className={utilStyles.loadingProductsCircle}></div>
+        <div className={utilStyles.ldsRing}></div>
+      </div> */}
+
+      <motion.div variants={cards} onPointerMove={(e) => moveCard(e)} onPointerLeave={(e) => leaveCard(e)} type='cardContainer' className={utilStyles.productCardContainer}>
 
                 <div type='card' className={utilStyles.productCard}>
                   <div className={utilStyles.ldsRing}></div>
@@ -145,6 +152,6 @@ export default function ProductsLoading(){
 
 
               </motion.div>
-        </>
-    )
+    </>
+  )
 }
